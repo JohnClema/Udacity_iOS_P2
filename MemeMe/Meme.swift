4871a3a34c5aa6155f90b7a297245c37c88aef60
John <john.clema@gmail.com>
2015-11-04 15:34:19 +0800

    feat: intial commit, project started with basic UI

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,16 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by John Clema on 3/11/2015.
+//  Copyright © 2015 JohnClema. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    var initalImage : UIImage
+    var editedImage : UIImage
+    var topText : String
+    var bottomText : String
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var albumsBarButtonItem: UIBarButtonItem!
+    @IBOutlet weak var cameraBarButtonItem: UIBarButtonItem!
+    
+    
+    var imageData : Meme?
+    var picker: UIImagePickerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: Selector("share:"))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Cancel, target: self, action: Selector("cancel:"))
+        
+        self.navigationController?.toolbarHidden = false
+        
+        
+//        self.setToolbarItems([camera, albums], animated: false)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +34,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func share(sender: UIBarButtonItem) {
+        print("Hello")
+    }
+    
+    func cancel(sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func openAlbums(sender: UIBarButtonItem) {
+        openPicker(sender, sourceType: .SavedPhotosAlbum)
+    }
+    
+    @IBAction func openCamera(sender: UIBarButtonItem) {
+        openPicker(sender, sourceType: .Camera)
+    }
+    
+    func openPicker(sender: UIBarButtonItem, sourceType: UIImagePickerControllerSourceType) {
+        self.picker = UIImagePickerController()
 
+        if(UIImagePickerController.isSourceTypeAvailable(sourceType))
+        {
+            self.picker!.sourceType = sourceType
+            self.picker?.delegate = self
+            self.picker!.mediaTypes = [kUTTypeImage as String]
+            self.picker!.allowsEditing = false
+
+            self.presentViewController(self.picker!, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print(info.description)
+        print("Image")
+        imageData?.initalImage = info.indexForKey("UIImagePickerControllerOriginalImage") as UIImage
+    }
 
 }
 
